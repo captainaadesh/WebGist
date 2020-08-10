45e1897529549cd7d3357b75d9eacd2c338d9ba9
Aadesh Mirajkar <mirajkaraadesh@gmail.com>
2020-08-10 15:25:00 +0530

    search function implemented

--- WebGist/routes.py
+++ WebGist/routes.py
@@ -1,24 +1,60 @@
 import requests
 
-from flask import Blueprint, render_template, current_app
+from isodate import parse_duration
+
+from flask import Blueprint, render_template, current_app, request
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
 	search_url = 'https://www.googleapis.com/youtube/v3/search'
+	video_url = 'https://www.googleapis.com/youtube/v3/videos'
 
-	search_params = {
-		'key' : current_app.config['YOUTUBE_API_KEY'],
-		'q' : 'flask',
-		'part': 'snippet',
-		'maxResults' : 3,
-		'type': 'video' 
-	}
+	videos = []
 
-	r = requests.get(search_url, params=search_params)
+	if request.method == 'POST':
 
-	print(r.json()['items'][0]['id']['videoId']) 
+		search_params = {
+			'key' : current_app.config['YOUTUBE_API_KEY'],
+			'q' : request.form.get('searchgist'),
+			'part': 'snippet',
+			'maxResults' : 6,
+			'type': 'video'
+		}
 
-	return render_template('index.html')
+		r = requests.get(search_url, params=search_params)
+
+		results = r.json()['items']
+
+		gists = []
+
+		for i in results:
+			gists.append(i['id']['videoId'])
+
+
+		video_params = {
+			'key' : current_app.config['YOUTUBE_API_KEY'],
+			'id' : ','.join(gists),
+			'part' : 'snippet, contentDetails',
+			'maxResults' : 6
+		}
+		
+		r = requests.get(video_url, params=video_params)
+
+		results = r.json()['items']
+
+
+		for i in results:
+
+			gist_data = {
+				'id' : i['id'],
+				'url' : f'https://www.youtube.com/watch?v={ i["id"] }',
+				'thumbnail' : i['snippet']['thumbnails']['high']['url'],
+				'duration' : int(parse_duration(i['contentDetails']['duration']).total_seconds()// 60),
+				'title' : i['snippet']['title'],
+			}
+			videos.append(gist_data)
+
+	return render_template('index.html', videos=videos)
