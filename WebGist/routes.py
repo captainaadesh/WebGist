dbe94ed7dd3f0df3115270c89a2bbf030480840e
Aadesh Mirajkar <mirajkaraadesh@gmail.com>
2020-08-10 12:36:56 +0530

    youtube api working fine

--- WebGist/routes.py
+++ WebGist/routes.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, render_template
+import requests
+
+from flask import Blueprint, render_template, current_app
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
+	search_url = 'https://www.googleapis.com/youtube/v3/search'
+
+	search_params = {
+		'key' : current_app.config['YOUTUBE_API_KEY'],
+		'q' : 'flask',
+		'part': 'snippet',
+		'maxResults' : 3,
+		'type': 'video' 
+	}
+
+	r = requests.get(search_url, params=search_params)
+
+	print(r.json()['items'][0]['id']['videoId']) 
+
 	return render_template('index.html')
